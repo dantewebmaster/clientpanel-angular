472af8a3646517911b7dcd61b1068b281702b9df
Dante Webmaster <dantewebmaster@outlook.com>
2018-01-11 22:02:49 -0200

    Dist criada e publicada, firebase info deleted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false,
-	firebase: {
-		apiKey: "AIzaSyA4JT6Lc1-5pHv8mAo--hb_DL_3ShvcvBE",
-		authDomain: "clientpanelapp-fe92b.firebaseapp.com",
-		databaseURL: "https://clientpanelapp-fe92b.firebaseio.com",
-		projectId: "clientpanelapp-fe92b",
-		storageBucket: "clientpanelapp-fe92b.appspot.com",
-		messagingSenderId: "713710202686"
-	}
+	production: false
 };
